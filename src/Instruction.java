41b481daea39ea26bf63a640dbad2b62a698b582
Sabrout <omarsabrout@gmail.com>
2015-12-02 00:39:46 +0200

    Heavily Modification for Instruction Class

--- src/Instruction.java
+++ src/Instruction.java
@@ -1,39 +1,45 @@
-public class Instruction {
-String operation ;
-int imm;
-int regA;
-int regB;
- public Instruction(String operation , int regA , int regB,int imm) {
-	this.operation = operation;
-	this.imm = imm;
-	this.regA = regA;
-	this.regB = regB;
-	
-}
-public String getOperation() {
-	return operation;
-}
-public void setOperation(String operation) {
-	this.operation = operation;
-}
-public int getImm() {
-	return imm;
-}
-public void setImm(int imm) {
-	this.imm = imm;
-}
-public int getRegA() {
-	return regA;
-}
-public void setRegA(int regA) {
-	this.regA = regA;
-}
-public int getRegB() {
-	return regB;
-}
-public void setRegB(int regB) {
-	this.regB = regB;
-}
- 
+public class Instruction
+{
+	String op;
+	int destReg;
+	int srcReg;
+	int srcReg2;
+	int immediate;
+
+	public Instruction(String op, int dest, int src, int unknown)
+	{
+		switch (op)
+		{
+		case "LW":
+			break;
+			
+		case "SW":
+			break;
+
+		case "JMP":
+			break;
+
+		case "BEQ":
+			break;
+
+		case "JALR":
+			break;
+			
+		case "RET":
+			break;
+			
+		case "ADD":
+		case "SUB":
+		case "NAND":
+		case "MUL":
+			break;
+			
+		case "ADDI":
+			break;
+			
+		default:
+			break;
+		}
+	}
 
 }
